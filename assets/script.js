d971de6adb5272dcf04aab2df9e0af820184a631
Pedro H. Rocha <pedro_6568@hotmail.com>
2022-12-22 19:37:49 -0300

    adicionando funcionalidades javascript

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+function clickMenu() {
+    if (itens.style.display == 'flex') {
+     itens.style.display = 'none'
+    }
+    else {
+     itens.style.display = 'flex'
+    }
+ }
+function rollPage1(){
+    window.scrollTo(0, 550);
+}
+function rollPage2(){
+    window.scrollTo(0, 1100);
+}
+function rollPage3(){
+    window.scrollTo(0, 2100);
+}
