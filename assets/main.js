98301b5577cbcbe0176d11dc6362bcc7e5e711f1
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-02-04 16:54:08 -0300

    alterando propriedades js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,8 @@
-alert('Site em construção...')
+//JS MENU
+var clkMenu = window.document.getElementsByClassName('material-symbols-outlined')[0]
+
+clkMenu.addEventListener('click', clickMenu)
+
 function clickMenu() {
     if (itens.style.display == 'flex') {
      itens.style.display = 'none'
@@ -7,8 +11,19 @@ function clickMenu() {
      itens.style.display = 'flex'
     }
  }
+
+ // JS RollPage
+var rollPag1 = window.document.getElementById('menu__li1')
+var rollPag2 = window.document.getElementById('menu__li2')
+var rollPag3 = window.document.getElementById('menu__li3')
+
+rollPag1.addEventListener('click', rollPage1)
+rollPag2.addEventListener('click', rollPage2)
+rollPag3.addEventListener('click', rollPage3)
+
+
 function rollPage1(){
-    window.scrollTo(0, 550);
+    window.scrollTo(0, 500)
 }
 function rollPage2(){
     window.scrollTo(0, 1100);
